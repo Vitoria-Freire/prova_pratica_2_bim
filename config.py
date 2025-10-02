9b79b51691673ed93d9dd23adf891d1e27fd6146
Vitória Freire <vitoriafs200703@gmail.com>
2025-10-02 11:29:57 -0300

    adicionada a tabela usuario no primeiro banco

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ import secrets
 class Config:
     SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///gerenciamento_futebol.db'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:labinfo@localhost:3306/futebol'
