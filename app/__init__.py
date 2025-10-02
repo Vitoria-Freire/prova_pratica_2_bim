40545c6ffc7db4481207aea577c8df849046984d
Vitória Freire <vitoriafs200703@gmail.com>
2025-10-02 11:49:07 -0300

    Adicionando configuracao para login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from config import Config
-from flask_wtf import CSRFProtect
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_wtf import CSRFProtect
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,11 +11,18 @@ app.config.from_object(Config)
 db = SQLAlchemy()
 csrf = CSRFProtect(app)
 migrate = Migrate()
+login_manager = LoginManager(app)
 
 db.init_app(app)
 migrate.init_app(app, db)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from app import routes, models
 
-with app.app_context():
-    db.create_all()
+@login_manager.user_loader
+def load_user(user_id):
+    return models.Usuario.query.get(int(user_id))
+
+# with app.app_context():
+#     db.create_all()
