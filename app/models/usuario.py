abe46b088774c5e66dd7b814665526c2fbe8f49c
Vitória Freire <vitoriafs200703@gmail.com>
2025-10-02 11:50:28 -0300

    Adicionando o UserMinin

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -2,8 +2,9 @@ from app import db
 import sqlalchemy as sa
 from typing import Optional
 import sqlalchemy.orm as so
+from flask_login import UserMixin
 
-class Usuario(db.Model):
+class Usuario(db.Model, UserMixin):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
