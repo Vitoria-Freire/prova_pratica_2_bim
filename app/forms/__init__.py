2b5e5f0fb73f9c9f0bc88bf2c2e08faafe06a3c0
Vitória Freire <vitoriafs200703@gmail.com>
2025-10-02 11:33:54 -0300

    Importando os formularios novos

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,5 @@ from .time_form import TimeForm
 from .jogador_form import JogadorForm
 from .treinador_form import TreinadorForm
 from .jogo_form import JogoForm
+from .login_form import LoginForm
+from .usuario_form import UsuarioForm
